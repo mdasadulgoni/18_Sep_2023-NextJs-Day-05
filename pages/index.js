44d4171ac1908aa2d8be781aaa60ecc51c877869
mdasadulgoni <mdasadulgoni@gmail.com>
2023-09-18 15:19:44 +0530

    c1

---
+++ pages/index.js
@@ -0,0 +1,28 @@
+
+// 1. Import Area
+import Head from 'next/head'
+
+// 2. Functions Defination Area
+function Home() {
+  return (
+
+    <>
+      <Head>
+        <title>Welcome to my first nextjs application</title>
+      </Head>
+      <header>
+
+      </header>
+      <main>
+      <aside></aside>
+      <section></section>
+      </main>
+      <footer>
+
+      </footer>
+    </>
+  )
+}
+
+// Export Area
+export default Home
